5c6e5e3af729319291ee8a82f9aeea13767d2e49
VictoriaT87 <victoriaemt@gmail.com>
2022-10-20 14:23:39 +0000

    remove unnecessary class in styles.css, change var to let in code in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -172,11 +172,11 @@ function hasTouch() {
 
 if (hasTouch()) { // remove all the :hover stylesheets
     try { // prevent exception on browsers not supporting DOM styleSheets properly
-        for (var si in document.styleSheets) {
-            var styleSheet = document.styleSheets[si];
+        for (let si in document.styleSheets) {
+            let styleSheet = document.styleSheets[si];
             if (!styleSheet.rules) continue;
 
-            for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+            for (let ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
                 if (!styleSheet.rules[ri].selectorText) continue;
 
                 if (styleSheet.rules[ri].selectorText.match(':hover')) {
