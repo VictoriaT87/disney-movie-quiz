5c21fa9636619ecb947c5e5a22fedd4682b673ac
VictoriaT87 <victoriaemt@gmail.com>
2022-10-22 17:15:18 +0000

    format script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,20 +33,20 @@ let instructionsBtn = document.getElementById("instructionsBtn");
 let span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-instructionsBtn.onclick = function() {
-  modal.style.display = "block";
+instructionsBtn.onclick = function () {
+    modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+span.onclick = function () {
+    modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
 
 /** 
